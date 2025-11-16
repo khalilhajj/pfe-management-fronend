781ec94fcb7ae0146b23f1fef9c584f222254391
khalilhajj <hajri.khalil@tek-up.de>
2025-11-16 20:47:37 +0100

    Update backend URL to production and add Snyk security rules to .gitignore

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://backend-pfe-management-production.up.railway.app/",
 });
 const token = localStorage.getItem("accessToken");
 export const login = async (credentials) => {
